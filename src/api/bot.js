69b3e161e6c6bbc37ade59229e1f702f5d05edc6
Tadanasi <tadanasi@gmail.com>
2021-06-01 18:06:06 +0300

    err handle

--- src/api/bot.js
+++ src/api/bot.js
@@ -1,26 +1,30 @@
-const https = require ( 'https' )
+const https = require ( 'https' );
 class Bot
 {
     constructor ( token )
     {
         this.token = token;
-    }
+    };
     get token ()
     {
         return this.token;
-    }
+    };
     req ( meth, params )
     {
         const options = {
             host: 'api.telegram.org',
             path: `/bot${this.token}/${meth}`,
             method: 'POST',
-            headers: { accept: "application/json" }
+            headers: { accept: 'application/json' }
         };
         const req = https.request ( options, ( res ) => {
-
+            const jsonres = JSON.parse ( res );
+            if ( !jsonres.ok ) console.log ( 'Telegram returned: ' + jsonres.error_code + '\n' + jsonres.description );
         } );
-    }
+        req.on ( 'error', ( err ) => {
+            console.log ( err ); //delete it later  
+        } );
+    };
 }
 
 export default Bot;
