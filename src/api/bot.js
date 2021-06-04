d95138fbe76d16de4951801d2eb77693ff7b5cd3
Tadanasi <tadanasi@gmail.com>
2021-06-04 16:36:33 +0300

    sender added

--- src/api/bot.js
+++ src/api/bot.js
@@ -9,21 +9,39 @@ class Bot
     {
         return this.token;
     };
-    req ( meth, params )
+    req ( meth, params = {} )
     {
+        params.accept = 'app;ication/json';
         const options = {
             host: 'api.telegram.org',
             path: `/bot${this.token}/${meth}`,
             method: 'POST',
-            headers: { accept: 'application/json' }
+            headers: params
         };
         const req = https.request ( options, ( res ) => {
             const jsonres = JSON.parse ( res );
             if ( !jsonres.ok ) console.log ( 'Telegram returned: ' + jsonres.error_code + '\n' + jsonres.description );
+            else return jsonres;
         } );
         req.on ( 'error', ( err ) => {
-            console.log ( err ); //delete it later  
+            console.log(err);
+            return;
         } );
+    };
+    send ( type, id, value, caption = null )
+    {
+        const params = {
+            id: id,
+            [type.toLowerCase()]: value
+        };
+        if ( caption ) params.caption = caption;
+        const result = this.req( `send${type}`, params );
+        if ( result ) return 'Succesfully sent';
+        else return 'Unexpected error';
+    };
+    sendMessage ( id, value, { caption } )
+    { 
+        return this.send('Message', id, value, caption);
     };
 }
 
